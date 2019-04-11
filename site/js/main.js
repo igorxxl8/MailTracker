1fc5355eb2701e14071cc6a21f1f09f4b69ddee4
IgorTurtsevich <igorturtsevich@coherentsolutions.com>
2019-04-11 11:29:59 +0300

    Added script for loading selectors

--- site/js/main.js
+++ site/js/main.js
@@ -1,38 +1,268 @@
-
-
 (function(){
+    var data = [
+        {
+            "Name": "Главный корпус БГУ",
+            "Profiles": [
+                {
+                    "Name": "МАТ",
+                    "Audiences": [
+                        {
+                            "Name": "517" 
+                        },
+                        {
+                            "Name": "517" 
+                        },
+                        {
+                            "Name": "513" 
+                        },
+                        {
+                            "Name": "606" 
+                        },
+                        {
+                            "Name": "609" 
+                        },
+                        {
+                            "Name": "433" 
+                        },
+                        {
+                            "Name": "521" 
+                        }
+                    ]
+                }
+            ], 
+        },
+        {
+            "Name": "Физический факультет БГУ",
+            "Profiles": [
+                {
+                    "Name": "ФИЗ",
+                    "Audiences": [
+                        {
+                            "Name": "319" 
+                        },
+                        {
+                            "Name": "319" 
+                        },
+                        {
+                            "Name": "213" 
+                        },
+                        {
+                            "Name": "211" 
+                        },
+                        {
+                            "Name": "321" 
+                        },
+                        {
+                            "Name": "418" 
+                        },
+                        {
+                            "Name": "220" 
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "Name": "Географический факультет БГУ",
+            "Profiles": [
+                {
+                    "Name": "Фил (РУС)",
+                    "Audiences": [
+                        {
+                            "Name": "115" 
+                        },
+                        {
+                            "Name": "312" 
+                        }
+                    ]
+                },
+                {
+                    "Name": "Фил (АНГ)",
+                    "Audiences": [
+                        {
+                            "Name": "116" 
+                        },
+                        {
+                            "Name": "212" 
+                        },
+                        {
+                            "Name": "311" 
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "Name": "Химический факультет БГУ",
+            "Profiles": [
+                {
+                    "Name": "ХИМ",
+                    "Audiences": [
+                        {
+                            "Name": "713" 
+                        },
+                        {
+                            "Name": "713" 
+                        },
+                        {
+                            "Name": "201" 
+                        },
+                        {
+                            "Name": "301" 
+                        },
+                        {
+                            "Name": "601" 
+                        }
+                    ]
+                }, 
+                {
+                    "Name": "ИМ",
+                    "Audiences": [
+                        {
+                            "Name": "705" 
+                        },
+                        {
+                            "Name": "705" 
+                        },
+                        {
+                            "Name": "706" 
+                        },
+                        {
+                            "Name": "707a" 
+                        },
+                        {
+                            "Name": "708" 
+                        },
+                        {
+                            "Name": "709" 
+                        },
+                        {
+                            "Name": "501" 
+                        },
+                        {
+                            "Name": "507" 
+                        },
+                        {
+                            "Name": "701" 
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "Name": "Юридический факультет БГУ",
+            "Profiles": [
+                {
+                    "Name": "БИО",
+                    "Audiences": [
+                        {
+                            "Name": "308" 
+                        },
+                        {
+                            "Name": "308" 
+                        },
+                        {
+                            "Name": "511" 
+                        },
+                        {
+                            "Name": "214" 
+                        },
+                        {
+                            "Name": "418" 
+                        },
+                        {
+                            "Name": "309" 
+                        },
+                        {
+                            "Name": "513" 
+                        },
+                        {
+                            "Name": "610" 
+                        },
+                        {
+                            "Name": "609" 
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "Name": "Факультет международных отношений БГУ",
+            "Profiles": [
+                {
+                    "Name": "ИСТ",
+                    "Audiences": [
+                        {
+                            "Name": "1202" 
+                        },
+                        {
+                            "Name": "1202" 
+                        },
+                        {
+                            "Name": "1201" 
+                        },
+                        {
+                            "Name": "1301" 
+                        },
+                        {
+                            "Name": "1302" 
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
     var buildingSelector = document.getElementById("building");
     var profileSelector = document.getElementById("profile");
     var audienceSelector = document.getElementById("audience");
-    var buildings = {
-        "Главный корпус БГУ": ["МАТ"], 
-        "Физический факультет БГУ": ["ФИЗ"], 
-        "Географический факультет БГУ": ["Фил (РУС)", "Фил (АНГ)"], 
-        "Химический факультет БГУ": ["ХИМ", "ИМ"], 
-        "Юридический факультет БГУ": ["БИО"], 
-        "Факультет международных отношений БГУ": ["ИСТ"]
-    };
-    var blank = document.createElement("option");
-    blank.text = ""
-    buildingSelector.appendChild(blank);
+    fillSelector(buildingSelector, data, "");
+    buildingSelector.addEventListener(
+        'change',
+        event => 
+        {
+            if (buildingSelector.options[0].text == ""){
+                buildingSelector.options[0] = null;
+            }
 
-    fillSelector(buildingSelector, Object.keys(buildings), { selector: profileSelector, info: buildings })
-})();
+            var index = event.target.value;
+            fillSelector(profileSelector, data[index].Profiles, "Все")
+            profileSelector.dispatchEvent(new Event('change'))
+        }
+    );
+    profileSelector.addEventListener(
+        'change',
+        event => 
+        {
+            var corpsIndex = buildingSelector.selectedIndex;
+            var index = event.target.value;
+            if (index == Number.MAX_SAFE_INTEGER){
+                var audiences = [];
+                for (item of data[corpsIndex].Profiles){
+                    audiences = audiences.concat(item.Audiences);
+                }
+                fillSelector(audienceSelector, audiences, "Все")
+            } else{
+                fillSelector(audienceSelector, data[corpsIndex].Profiles[index].Audiences, "Все")
+            }
+        }
+    );
+    
+    function fillSelector(selector, data, first = null){
+        for (index in selector.options){
+            selector.options[index] = null;
+        }
 
-
-function fillSelector(selector, values, related = null){
-    values.forEach(item => {
-        var option = document.createElement("option");
-        option.text = item.toString();
-        selector.appendChild(option);
-    })
-    if (related){
-        selector.addEventListener(
-            "change", 
-            () => {
-                
-                var opt = selector.options[selector.selectedIndex].value;
-                fillSelector(related.selector, related.info[opt])  
-            })
+        if (first != null) {
+            var option = new Option();
+            option.text = first;
+            option.value = Number.MAX_SAFE_INTEGER;
+            selector.add(option);
+        }
+        for (index in data){
+            var option = new Option();
+            option.text = data[index].Name;
+            option.value = index;
+            selector.add(option);
+        }
     }
-}
+})();
