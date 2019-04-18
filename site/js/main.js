63db62966167f7e61fbfdc160a6c2035467db4ef
IgorTurtsevich <igorturtsevich@coherentsolutions.com>
2019-04-18 08:16:00 +0300

    Correct data loading

--- site/js/main.js
+++ site/js/main.js
@@ -1,5 +1,6 @@
 (() => {
     controller(getApi())
+
     function getApi(){
         const fetchGet = async(url) => await(await fetch(url)).json();
         return {
@@ -8,17 +9,43 @@
             getStudents:   (corps = "", place = "") => fetchGet(`https://lyceumexams.herokuapp.com/api/pupils?corps=${corps}&place=${place}`)
         }
     }
-    
 
     function controller(api) {
-        api.getDictionary().then(onDictionaryGet);
+        var students = [];
+        var filtered = [];
+        var corpses = [];
+        var studentsTable = document.getElementById("students");
+        var corpsesSelector = document.getElementById("building");
 
-        function onCorpsesGet(corpses){
-            var corpsesSelector = document.getElementById("building");
+        api.getDictionary().then(onDictionaryGet);
+        api.getCorpses().then(onCorpsesGet);
+        init();
+        
+        function onCorpsesGet(corpsesData){
+            corpses = corpsesData;
+            fillSelector(corpsesSelector, corpses, "name", "");
+        }
+        
+        function onDictionaryGet(dict){
+            Mapper.dictionary = dict;
+        }
+        
+        function onStudentsGet(studentsData){
+            students = studentsData;
+            filtered = students;
+            fillStudentsTable(studentsTable, students);
+        }
+
+        function init(){
             var placeSelector = document.getElementById("profile");
             var audienceSelector = document.getElementById("audience");
-            fillSelector(corpsesSelector, corpses, "name", "");
+            var serchTextField = document.getElementById("search");
+            var tHeader = document.getElementById("tHeader");
             var selectedCorps;
+            var attrs;
+            var shouldSwitch;
+            var asc;
+
             corpsesSelector.addEventListener(
                 'change',
                 event => 
@@ -29,8 +56,10 @@
                     selectedCorps = corpses[event.target.value];
                     fillSelector(placeSelector, selectedCorps.places, "code", "Все")
                     placeSelector.dispatchEvent(new Event('change'));
+                    serchTextField.value = '';
                 }
             );
+
             placeSelector.addEventListener(
                 'change',
                 event => 
@@ -48,43 +77,32 @@
                         stud = api.getStudents(selectedCorps.alias, selectedCorps.places[index]._id);
                     }
                     stud.then(onStudentsGet);
-                    fillSelector(audienceSelector, audiences, "name", "Все")
+                    fillSelector(audienceSelector, audiences, "name", "Все");
                 }
             );
 
-            function fillSelector(selector, corpses, attr,  current = null){
-                for (index in selector.options){
-                    selector.options[index] = null;
-                }
-        
-                if (current != null) {
-                    var option = new Option();
-                    option.text = current;
-                    option.value = Number.MAX_SAFE_INTEGER;
-                    selector.add(option);
-                }
-        
-                for (index in corpses){
-                    var option = new Option();
-                    option.text = corpses[index][attr];
-                    option.value = index;
-                    selector.add(option);
-                }
-            }
-        }
+            audienceSelector.addEventListener(
+                'change',
+                () => {
+                    filtered = students;
+                    var index = event.target.selectedIndex;
+                    if (index != 0){
+                        var audience = audienceSelector[index].text;
+                        filtered = students.filter(s => Mapper.audience(s.audience) === audience)
+                    }
+                    fillStudentsTable(studentsTable, filtered);
+                }  
+            );
 
-        function onDictionaryGet(dict){
-            Mapper.dictionary = dict;
-            api.getCorpses().then(onCorpsesGet);
-        }
-
-        var studentsTable = document.getElementById("students");
-        var tHeader = document.getElementById("tHeader");
-        function onStudentsGet(students){
-            fillStudentsTable();
-            var attrs;
-            var shouldSwitch;
-            var asc;
+            serchTextField.addEventListener(
+                'input',
+                () => {
+                    var text = (event.target.value).replace(/ /g, '').toLowerCase();
+                    var temp = filtered.filter(s => (s.firstName + s.lastName + s.parentName).toLowerCase().includes(text));
+                    fillStudentsTable(studentsTable, temp);
+                }
+            )
+            
             tHeader.addEventListener(
                 'click',
                 () => {
@@ -93,67 +111,92 @@
                     if (shouldSwitch){
                         asc = 1;
                     }
-        
+
                     attrs = dataset.attrs;
-                    sortData(students, attrs, dataset.type);
-                    fillStudentsTable();
-                    appendArrow();
+                    
+                    if (!shouldSwitch){
+                        asc *= -1;
+                    }
+                    sortData(students, asc, attrs, dataset.type);
+                    fillStudentsTable(studentsTable, students);
+                    appendArrow(asc);
                 }
             );
-                
-            function appendArrow(){
-                var arrowUp = document.getElementsByClassName('arrow-up')[0];
-                var arrowDown= document.getElementsByClassName('arrow-down')[0];
-                if (arrowUp != null){
-                    arrowUp.className = "";
-                }
-                if (arrowDown != null){
-                    arrowDown.className = "";
-                }
-                
-                var arrowStyle = 'arrow-down';
-                if (asc == 1){
-                    arrowStyle = 'arrow-up';
-                }
-                event.target.className = arrowStyle;
-            }
-            
-            function sortData(corpses, attrs, type) {
-                if (!shouldSwitch){
-                    asc *= -1;
-                }
-                corpses.sort(predicate);
-        
-                function predicate(current, next){
-                    var a = new Convertible(current, attrs)[type];
-                    var b = new Convertible(next, attrs)[type];
-                    var returnValue = -1;
-                    if (a > b){
-                        returnValue = 1;
-                    }
-                    if (a == b){
-                        returnValue = 0;
-                    }
-                    return asc*returnValue;
-                }
-            }
-        
-            function fillStudentsTable(){
-                var body = document.createElement('tbody');
-                for (index in students){
-                    var student = students[index];
-                    var row = document.createElement('tr');
-                    row.draggable = true;
-                    row.innerHTML = `<td>${++index}</td>
-                                    <td>${student.firstName + ' ' + student.lastName + ' ' + student.parentName }</td>
-                                    <td>${Mapper.dictionary.audiences[student.audience]}</td>
-                                    <td>${Mapper.dictionary.profiles[student.profile]}</td>
-                                    <td>${student.needBel}</td>`
-                    body.appendChild(row);
-                }
-                studentsTable.replaceChild(body, studentsTable.tBodies[0])
-            }
         }
+    }
+    
+    function sortData(data, asc, attrs, type) {
+        data.sort(predicate);
+        
+        function predicate(current, next){
+            var a = new Convertible(current, attrs)[type];
+            var b = new Convertible(next, attrs)[type];
+            var returnValue = -1;
+            if (a > b){
+                returnValue = 1;
+            }
+            if (a == b){
+                returnValue = 0;
+            }
+            return asc*returnValue;
+        }
+    }
+
+    function deleteArrow(){
+        var arrowUp = document.getElementsByClassName('arrow-up')[0];
+        var arrowDown= document.getElementsByClassName('arrow-down')[0];
+        if (arrowUp != null){
+            arrowUp.className = "";
+        }
+        if (arrowDown != null){
+            arrowDown.className = "";
+        }
+    }
+
+    function appendArrow(asc){
+        deleteArrow();
+        
+        var arrowStyle = 'arrow-down';
+        if (asc == 1){
+            arrowStyle = 'arrow-up';
+        }
+        event.target.className = arrowStyle;
+    }
+
+    function fillSelector(selector, data, attr,  current = null){
+        for (index in selector.options){
+            selector.options[index] = null;
+        }
+
+        if (current != null) {
+            var option = new Option();
+            option.text = current;
+            option.value = Number.MAX_SAFE_INTEGER;
+            selector.add(option);
+        }
+
+        for (index in data){
+            var option = new Option();
+            option.text = data[index][attr];
+            option.value = index;
+            selector.add(option);
+        }
+    }
+
+    function fillStudentsTable(table, students){
+        var body = document.createElement('tbody');
+        for (index in students){
+            var student = students[index];
+            var row = document.createElement('tr');
+            row.draggable = true;
+            row.innerHTML = `<td>${++index}</td>
+                            <td>${student.firstName + ' ' + student.lastName + ' ' + student.parentName }</td>
+                            <td>${Mapper.audience(student.audience)}</td>
+                            <td>${Mapper.profile(student.profile)}</td>
+                            <td>${student.needBel}</td>`
+            body.appendChild(row);
+        }
+        table.replaceChild(body, table.tBodies[0])
     }
 })();
 
@@ -175,8 +218,8 @@ class Convertible {
 }
 
 class Mapper {
+    static default  = val  => val;
+    static audience = val  => this.dictionary.audiences[val];
+    static profile  = val  => this.dictionary.profiles[val];
     static map      = prop => this.hasOwnProperty(prop) ? this[prop] : this.default;
-    static default  = val => val;
-    static audience = val => this.dictionary.audiences[val];
-    static profile  = val => this.dictionary.profiles[val];
 }
