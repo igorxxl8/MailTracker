82b71cdbeea23f2a0b37e287b0ac6818727564c6
igorxxl <vip.turcevich3@gmail.com>
2019-05-13 11:33:29 +0300

    lab9 completed

--- js-labs/js/lab9/individual/task1.js
+++ js-labs/js/lab9/individual/task1.js
@@ -1,14 +1,120 @@
 function ind_task1(){
+    class Tuple2{
+        constructor(item1, item2){
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        toString() {
+            return `(${this.item1}, ${this.item2})`
+        }
+    }
+
     class TaskList {
-        constructor(tasks){
+        constructor(tasks=[]){
             this.tasks = tasks;
+        }
+
+        addTask(task){
+            if (!task instanceof Task) return;
+            this.tasks.push(task);
+        }
+
+        removeTask(task){
+            var tasks = this.tasks;
+            for (var index in tasks){
+                if (tasks[index] == task) 
+                    delete tasks[index];
+            }
+        }
+
+        findTaskByAttr(attr,value){
+            var tasks = this.tasks;
+            for (var index in tasks){
+                if (tasks[index][attr] != value) continue;
+                return tasks[index];
+            }
+        }
+
+        correct(job, data) {
+            var task = this.findTaskByAttr("job", job);
+
+            if (data.project_name !== undefined)
+                task.project_name = data.project_name;
+            if (data.job !== undefined)
+                task.job = data.job;
+            if (data.lead !== undefined)
+                task.lead = data.lead;
+            if (data.performer !== undefined)
+                task.performer = data.performer;
+            if (data.start !== undefined)
+                task.start = data.start;
+            if (data.end !== undefined)
+                task.end = data.end;
+            alert(task);
+        }
+
+        sortByProjectName(){
+            this.sort('project_name');
+        }
+
+        sortByTaskName(){
+            this.sort('job');
+        }
+
+        sortByLead(){
+            this.sort('lead');
+        }
+
+        sortByPerformer(){
+            this.sort('performer');
+        }
+
+        sortByStart(){
+            this.sort('start');
+        }
+
+        sortByEnd(){
+            this.sort('end');
+        }
+            
+        sort(attr) {
+            this.tasks.sort(preducate);
+
+            function preducate(current, next) {
+                var a = current[attr];
+                var b = next[attr];
+                var returnValue = -1;
+                if (a > b){
+                    returnValue = 1;
+                }
+                if (a == b){
+                    returnValue = 0;
+                }
+                return returnValue;
+            }
         }
     
         getTasksByProject(project_name){
-            return this.tasks.filter(p => p.project_name == project_name)
+            var filtered = this.tasks.filter(p => p.project_name == project_name)
+            var result = filtered.map(t => new Tuple2(`Task: ${t.job}`, `Performer: ${t.performer}`));
+            return result;
+        }
+
+        getTasksWithComingMonthDeadline() {
+            var date = new Date();
+            var nextMonth = new Date();
+            nextMonth.setMonth((date.getMonth()+1)%12);
+            return this.tasks.filter(t => +date <= +t.end && +t.end <= +nextMonth);
+        }
+
+        toString() {
+            return this.tasks.toString();
         }
     }
     
+
+
     class Task {
         constructor(project_name, job,  lead, performer, start, end){
             this.project_name = project_name;
@@ -21,32 +127,69 @@ function ind_task1(){
         }
     
         toString(){
-            return `Project: ${this.project_name}
+            return `\nProject: ${this.project_name}
                     job: ${this.job}
                     lead: ${this.lead}
                     performer: ${this.performer}
                     start: ${this.start.toLocaleString()}
-                    end: ${this.end.toLocaleString()}\n`
+                    end: ${this.end.toLocaleString()}`
         }
     }  
 
-    var task1 = new Task("Proj", "Make graphic", "Ivanov Ivan", "Turcevich Igor", new Date(), new Date());
-    var task2 = new Task("Proj", "Make cake", "Ivanov Ivan", "Petrov", new Date(), new Date());
-    var tasks = new TaskList([task1, task2]);
-    var projTasks = tasks.getTasksByProject("Proj");
-    alert(projTasks)
+    var proj_name1 = "Proj";
+    var proj_name2 = "Another";
+    var date = new Date();
+    var end1 = new Date();
+    end1.setMonth((date.getMonth()+1)%12);
+    var end2 = new Date();
+    end2.setMonth((date.getMonth()+2)%12)
+    var task1 = new Task(proj_name1, "Make graphic", "Ivanov Ivan", "Turcevich Igor", date, end1);
+    var task2 = new Task(proj_name1, "Make cake", "Ivanov Ivan", "Petrov", date, end1);
+    var task3 = new Task(proj_name2, "Make task", "Ivanov Ivan", "Turcevich Igor", date, end2);
+    var task4 = new Task(proj_name2, "Make tree", "Genrich", "Petrov", date, end2);
+    var tasks = new TaskList([task1, task2, task3]);
+    tasks.addTask(task4);
+    var projTasks = tasks.getTasksByProject(proj_name1);
+    alert(`Project: ${proj_name1}:\nTasks: ${projTasks}`)
 
+    tasks.removeTask(task2);
+    var projTasks2 = tasks.getTasksByProject(proj_name1);
+    alert(`Project: ${proj_name1}:\nTasks: ${projTasks2}`)
+
+    var test = tasks.getTasksByProject(proj_name2);
+    alert(`Project: ${proj_name2}:\nTasks: ${test}`)
     
+    var tasksMonth = tasks.getTasksWithComingMonthDeadline();
+    alert(`Comming month deadline: ${tasksMonth}`);
+
+    tasks.sortByProjectName();
+    alert(`Sorted by project name: ${tasks}`);
+
+    tasks.sortByTaskName();
+    alert(`Sorted by task name: ${tasks}`);
+
+    tasks.sortByLead();
+    alert(`Sorted by lead: ${tasks}`);
+
+    tasks.sortByPerformer();
+    alert(`Sorted by performer: ${tasks}`);
+
+    tasks.sortByStart();
+    alert(`Sorted by start: ${tasks}`);
+
+    tasks.sortByEnd();
+    alert(`Sorted by end: ${tasks}`);
+    
+    tasks.correct(
+        "Make tree", 
+        {
+            "job":"Make graphic", 
+            "lead":"Ivanov Ivan",
+            "performer": "Turcevich Igor", 
+            "start":date, 
+            "end":end1
+        }
+    )
+
+    alert(tasks);
 }
-
-
-//Использовать массив объектов класса, 
-//который содержит данные в соответствии с заданием. 
-//В каждом варианте задания реализовать следующие 
-//функции класса: 
-//инициализации данных класса, 
-//добавления, 
-//удаления, 
-//корректировки, 
-//сортировки  и 
-//просмотра записей.  
